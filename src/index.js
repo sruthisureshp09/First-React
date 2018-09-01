6535f9411d13d672a5824a52009174dfa05bb6d2
Sruthi <sruthisureshp09@gmail.com>
2018-09-01 12:36:19 +0530

    PropsandComponents

--- src/index.js
+++ src/index.js
@@ -1,24 +1,37 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-
-/* let clock= function()
+ /* function Welcome(props)
 {
-   return <h2>this is clocktime {new Date().toLocaleTimeString()} </h2> 
+    return <h1> Hello, {props.name} </h1>
+}  */
+
+/* class Welcome extends React.Component{
+    render() {
+        return <h1> Hello, {this.props.name} </h1>
+
+    }
+} */   // using esf class
+function Welcome(props)
+{
+    return <h1> Hello, {props.name}  on {props.place} </h1>   
 }
-ReactDOM.render(
-  
- clock(),
 
-    document.getElementById('root')
-); */
 
-setInterval(function(){
-   
+function Show()
+{
+    return <div>
+               <Welcome name='Sruthi' place='vadakara' /> 
+               <Welcome name='Athulya' place='tirur' />
+
+           </div>
+}
+
     ReactDOM.render(
   
-     <h2>The Time is {new Date().toLocaleTimeString()} </h2>, 
+    //<Welcome name='Sruthi' />
+    Show(),
      document.getElementById('root')
        );
-},1000);
+
 
