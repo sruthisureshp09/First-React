e383168b84382f1ba405046764c3e66d2cead6ab
Sruthi <sruthisureshp09@gmail.com>
2018-09-01 11:54:25 +0530

    clock app

--- src/index.js
+++ src/index.js
@@ -1,19 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-function myfunction(name='sruthi',place='vadakara')
+
+/* let clock= function()
 {
-    return <div> 
-                <h2>My function returns my name <b>{name}</b> and place <b>{place} </b></h2>
-                <h1>helloooo drr</h1>
-            </div>
+   return <h2>this is clocktime {new Date().toLocaleTimeString()} </h2> 
 }
-
-
 ReactDOM.render(
   
-    myfunction('Noname','Noplace'),
+ clock(),
 
     document.getElementById('root')
-);
+); */
+
+setInterval(function(){
+   
+    ReactDOM.render(
+  
+     <h2>The Time is {new Date().toLocaleTimeString()} </h2>, 
+     document.getElementById('root')
+       );
+},1000);
 
